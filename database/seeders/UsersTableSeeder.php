4e191629b0d53b5c99f2909c4711e04adb3095b5
Matheus Victor <matheus@primeinternacional.com.br>
2022-04-25 20:39:54 -0300

    Criado a seed do usuário ADMIN

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $usuarioAdmin = new User();
+
+        $usuarioAdmin->name = "Admin Café Contri";
+        $usuarioAdmin->email = "admin@cafecontri.com.br";
+        $usuarioAdmin->password = Hash::make('admin@cafecontri123');
+        $usuarioAdmin->tipo = "PESSOA_JURIDICA";
+        $usuarioAdmin->tipo_permissao = "ADMIN";
+
+        $usuarioAdmin->save();
+    }
+}
